a36665540475d0922ff6d7906f4098ceac9d6494
ejackson3 <31971518+ejackson3@users.noreply.github.com>
2017-10-23 23:22:11 -0500

    Add files via upload

---
+++ jQuery (hw)/jquery/myscript.js
@@ -0,0 +1,12 @@
+//I put everything in this function.
+
+console.log('script file loaded...');
+
+$(document).ready {function() {
+
+	console.log('in my doc ready f/n');
+
+	$('#my-div').addClass('my-border');
+
+
+});
